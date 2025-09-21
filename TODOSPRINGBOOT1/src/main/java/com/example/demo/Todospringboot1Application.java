19336ea7dd14ad95d0789cbe8935dbbd33d6d044
vijay <vijaykollu6@gmail.com>
2025-09-21 16:50:36 +0530

    updated

--- TODOSPRINGBOOT1/src/main/java/com/example/demo/Todospringboot1Application.java
+++ TODOSPRINGBOOT1/src/main/java/com/example/demo/Todospringboot1Application.java
@@ -2,13 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Todospringboot1Application extends SpringBootServletInitializer{
+public class Todospringboot1Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Todospringboot1Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Todospringboot1Application.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Todospringboot1Application.class, args);
+    }
 }
